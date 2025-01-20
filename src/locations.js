a30237a63ea298c7d97a31445c54b2634b9ea158
Oleksandr Zhmurko <oleksmurke@gmail.com>
2025-01-20 21:09:02 +0200

    Nothing special

--- src/locations.js
+++ src/locations.js
@@ -1,11 +1,10 @@
-import { createElement, updateDialogBox } from "./utility";
-import { character } from "./char";
+import { createElement, updateDialogBox, updateInstructions } from "./utility.js";
+import { character } from "./char.js";
 
 export const BaseZone = {
     elements: {
         gameContainer: document.getElementById("game-container"),
         dialogBox: document.getElementById("dialog-box"),
-        instructions: document.getElementById("instructions"),
         characterInfo: createElement("div", { 
             properties: { textContent: JSON.stringify(character, null, 2) } }),
         campfireGif: createElement("img", {
@@ -13,13 +12,11 @@ export const BaseZone = {
             properties: { src: "assets/animations/winterCamp.gif" }})
     },
     render() {
-        const { gameContainer, dialogBox, creationArea, characterInfo, campfireGif } = this.elements;
+        const { gameContainer, dialogBox, characterInfo, campfireGif } = this.elements;
 
+        updateInstructions("Base Zone")
         updateDialogBox("Welcome, traveler!");
         gameContainer.insertBefore(characterInfo, dialogBox);
         gameContainer.insertBefore(campfireGif, dialogBox);
-        // add an animated pixelated bonfire (possibly in dark souls style)
-        // render basic stats as a top bar: name, class, hp, lvl, exp, settings button (save game, load game, main menu)
-        // add buttons: (1) view detailed stats, (2) inventory
     }
 }
